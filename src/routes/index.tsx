526f7590e44e28ea0e3217fcaec659e111482076
Matheus <matheusandrade.ma2003@gmail.com>
2025-10-24 19:47:33 -0300

    feat: add profile at router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,12 +3,12 @@ import { Routes, Route, Navigate } from "react-router";
 import { CardsDetailPage, DashboardPage, LoginPage } from "@/pages";
 import { Layout } from "@/pages/layout";
 import { PrivateRoute } from "@/components/PrivateRoute";
+import Perfil from "@/pages/Perfil/Perfil";
 
 export function AppRoutes() {
   return (
     <Routes>
       <Route path="/" element={<LoginPage />} />
-
       <Route
         path="/dashboard"
         element={
@@ -19,6 +19,7 @@ export function AppRoutes() {
       >
         <Route index element={<DashboardPage />} />
         <Route path="cards/detail/:id" element={<CardsDetailPage />} />
+        <Route path="profile" element={<Perfil />} />
       </Route>
       <Route path="*" element={<Navigate to="/" replace />} />
     </Routes>
