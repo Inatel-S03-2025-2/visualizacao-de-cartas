11ab7ded26b05954458eb19ca662b276d9bcf0e3
joaovictorgs <joaovictorgodoydasilva@gmail.com>
2025-10-31 18:20:19 -0300

    feat: adding routing the main page when click on the logo  on the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,20 @@
 import { UserDropdown } from "@/components/UserDropdown";
 import styles from "./styles.module.css";
 import logo from "@/assets/logo.png";
+import { useNavigate } from "react-router";
 
 export function Header() {
+  const navigate = useNavigate();
   return (
     <header className={styles.container}>
       <div className={styles.headerContent}>
         <div className={styles.container_logo}>
-          <img src={logo} alt="Logo" />
+          <img
+            src={logo}
+            alt="Logo"
+            style={{ cursor: "pointer" }}
+            onClick={() => navigate("/dashboard")}
+          />
           <h1>Visualização de cartas</h1>
         </div>
 
