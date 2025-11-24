746ac5580649a81b24990d6ebeb935eb9e1c8117
unknown <jvbala2014@gmail.com>
2025-11-24 20:08:52 -0300

    Feat: Novo_Perfil

--- src/pages/Perfil/Perfil.tsx
+++ src/pages/Perfil/Perfil.tsx
@@ -1,4 +1,3 @@
-
 import styles from "./Perfil.module.css";
 
 const Profile: React.FC = () => {
@@ -8,48 +7,63 @@ const Profile: React.FC = () => {
     victories: 128,
     defeats: 45,
     favoritePokemon: "Pikachu",
-    avatar: "https://archives.bulbagarden.net/media/upload/8/8a/Ash_SM.png",
+    avatar:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/trainers/ash.png",
     pokemons: [
-      { name: "Charizard", defeated: 35, image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png" },
-      { name: "Bulbasaur", defeated: 20, image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" },
-      { name: "Squirtle", defeated: 18, image: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/7.png" },
-    ],
+      { name: "Pikachu", defeated: 12, sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png" },
+      { name: "Charizard", defeated: 27, sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/6.png" },
+      { name: "Blastoise", defeated: 19, sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/9.png" },
+      { name: "Venusaur", defeated: 15, sprite: "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png" },
+    ]
   };
 
   return (
-    <div className={styles.container}>
-      {/* Coluna esquerda com informações do jogador */}
-      <aside className={styles.sidebar}>
-        <img src={player.avatar} alt="Foto do jogador" className={styles.avatar} />
-        <h2 className={styles.name}>{player.name}</h2>
-        <p className={styles.email}>{player.email}</p>
+    <div className={styles.pageBackground}>
+      <div className={styles.profileContainer}>
 
-        <div className={styles.stats}>
-          <div>
-            <span className={styles.label}>Vitórias:</span>
-            <span>{player.victories}</span>
+        
+        <div className={styles.leftCard}>
+          <img
+            src={player.avatar}
+            alt="Avatar do jogador"
+            className={styles.avatar}
+          />
+
+          <h2>{player.name}</h2>
+          <p className={styles.email}>{player.email}</p>
+
+          <div className={styles.infoBox}>
+            <span>Vitórias</span>
+            <strong>{player.victories}</strong>
           </div>
-          <div>
-            <span className={styles.label}>Derrotas:</span>
-            <span>{player.defeats}</span>
+
+          <div className={styles.infoBox}>
+            <span>Derrotas</span>
+            <strong>{player.defeats}</strong>
           </div>
-          <div>
-            <span className={styles.label}>Pokémon Preferido:</span>
-            <span>{player.favoritePokemon}</span>
+
+          <div className={styles.infoBox}>
+            <span>Pokémon preferido: </span>
+            <strong className={styles.favoritePokemon}>{player.favoritePokemon}</strong>
           </div>
         </div>
-      </aside>
 
-      {/* Cards dos Pokémons */}
-      <main className={styles.pokemonSection}>
-        {player.pokemons.map((pokemon, index) => (
-          <div key={index} className={styles.pokemonCard}>
-            <img src={pokemon.image} alt={pokemon.name} className={styles.pokemonImage} />
-            <h3 className={styles.pokemonName}>{pokemon.name}</h3>
-            <p className={styles.defeated}>Pokémons derrotados: {pokemon.defeated}</p>
+        
+        <div className={styles.rightContent}>
+          <h2 className={styles.sectionTitle}>Meus Pokémons</h2>
+
+          <div className={styles.pokemonGrid}>
+            {player.pokemons.map((p) => (
+              <div key={p.name} className={styles.pokemonCard}>
+                <img src={p.sprite} className={styles.pokemonImage} />
+                <h3>{p.name}</h3>
+                <p>Pokémons derrotados: <strong>{p.defeated}</strong></p>
+              </div>
+            ))}
           </div>
-        ))}
-      </main>
+
+        </div>
+      </div>
     </div>
   );
 };
